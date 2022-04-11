f2ccc319cb0fa5bdf1a1f1bf3afd6623f4ea69fe
sushantasinha <25884819+sushantasinha@users.noreply.github.com>
2022-04-11 21:22:29 +0530

    apache camel tutorials (v2)

--- project-a/src/main/java/com/project/a/demo/ActiveMqSenderRouter.java
+++ project-a/src/main/java/com/project/a/demo/ActiveMqSenderRouter.java
@@ -7,9 +7,19 @@ import org.springframework.stereotype.Component;
 public class ActiveMqSenderRouter extends RouteBuilder {
     @Override
     public void configure() throws Exception {
-        from("timer:active-mq-timer>period=10000")
+
+        //Uncomment to test
+       /* from("timer:active-mq-timer>period=10000")
                 .transform().constant("My activeMQ Message")
                 .log("${body}")
+                .to("activemq:my-active-queue");*/
+
+
+        //Now pick files from local and send to activemq
+        ///This will moved .camel folder under, after processing, under "C://input/json", where we could see all processed files.
+        from("file:C://input/json")
+                .log("${body}")
                 .to("activemq:my-active-queue");
+
     }
 }
--- project-b/src/main/java/com/project/b/demo/ActiveMqReceiverRouter.java
+++ project-b/src/main/java/com/project/b/demo/ActiveMqReceiverRouter.java
@@ -1,13 +1,78 @@
 package com.project.b.demo;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.model.dataformat.JsonLibrary;
 import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
 
 @Component
 public class ActiveMqReceiverRouter extends RouteBuilder {
     @Override
     public void configure() throws Exception {
         from("activemq:my-active-queue")
+                .unmarshal().json(JsonLibrary.Jackson, CurrencyExchange.class)
                 .to("log:received-message-from-activemq");
+    }
+
+}
+
+/*@AllArgsConstructor
+@NoArgsConstructor*/
+//@ToString
+//Lombok Annotation did nto work, so generated getters, setters, constructor etc
+//Did not analyse why failing
+class CurrencyExchange {
+
+    public CurrencyExchange() {
+
+    }
+
+    public CurrencyExchange(String from, String to, BigDecimal conversionRate) {
+        this.from = from;
+        this.to = to;
+        this.conversionRate = conversionRate;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public BigDecimal getConversionRate() {
+        return conversionRate;
+    }
+
+    private String from;
+    private String to;
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public void setTo(String to) {
+        this.to = to;
+    }
+
+    public void setConversionRate(BigDecimal conversionRate) {
+        this.conversionRate = conversionRate;
+    }
+
+    private BigDecimal conversionRate;
+
+    @Override
+    public String toString() {
+        return "CurrencyExchange{" +
+                "from='" + from + '\'' +
+                ", to='" + to + '\'' +
+                ", conversionRate=" + conversionRate +
+                '}';
     }
 }
