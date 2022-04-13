935a43866218958f4e45efdb52a1c237f30fb446
sushantasinha <25884819+sushantasinha@users.noreply.github.com>
2022-04-13 11:25:09 +0530

    apache camel tutorials (v5)

--- project-a/src/main/java/com/project/a/demo/MyFileRouter.java
+++ project-a/src/main/java/com/project/a/demo/MyFileRouter.java
@@ -5,10 +5,33 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class MyFileRouter extends RouteBuilder {
+   /* @Override
+    public void configure() throws Exception {
+        from("file:C:/input")
+                .routeId("Files-Input-Route")
+                .log("${body}") //printing the content of the body
+                .to("file:C:/output");
+    }*/
+
+    //choice() and Simple Language
     @Override
     public void configure() throws Exception {
         from("file:C:/input")
+                .routeId("Files-Input-Route")
+                .choice()
+                    .when(simple("${file:ext} ends with 'xml'"))//simple language
+                    //.when(simple(${file:ext} == 'xml'")
+                        .log("XML File")
+                    .when(simple("${body} contains 'INR'"))//simple language
+                        .log("JSON File and contains INR")
+                    .otherwise()
+                        .log("NOT XML or JSON file")
+                .end()
                 .log("${body}") //printing the content of the body
+                .log("${messageHistory}")//entire message details incl. headers will be printed
+                //.log("${messageHistory} ${headers}")//All headers will be printed
+                //.log("${messageHistory} ${headers.CamelFileAbsolutePath}")//specific headers will be printed
+                //.log("${messageHistory} ${file.absolute.path}")//same result as line 32 (previous line)
                 .to("file:C:/output");
     }
 }
